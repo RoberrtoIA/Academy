da624446eb3b3ac7b88f4ed3efcdc6505a4efa3e
Roberto Moreno <rmoreno@applaudostudios.dev>
2022-11-02 17:40:26 -0600

    Program form request edit

--- app/Http/Requests/StoreProgramRequest.php
+++ app/Http/Requests/StoreProgramRequest.php
@@ -3,17 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Auth;
 
-<<<<<<< HEAD
 class StoreProgramRequest extends FormRequest
-=======
-<<<<<<<< HEAD:app/Http/Requests/ModuleRequest.php
-class ModuleRequest extends FormRequest
-========
-class StoreProgramRequest extends FormRequest
->>>>>>>> 0a925481b51cfebeb56a832de392919b4a9cc15c:app/Http/Requests/StoreProgramRequest.php
->>>>>>> 0a925481b51cfebeb56a832de392919b4a9cc15c
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,7 +13,7 @@ class StoreProgramRequest extends FormRequest
      */
     public function authorize()
     {
-        return Auth::check();
+        return true;
     }
 
     /**
@@ -33,13 +24,9 @@ class StoreProgramRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required|max:100',
-            'description' => 'required|max:200',
-            'content' => 'required|min:1',
+            'title' => 'filled|max:100',
+            'description' => 'filled|max:200',
+            'content' => 'filled|min:1',
         ];
     }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 0a925481b51cfebeb56a832de392919b4a9cc15c
