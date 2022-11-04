fda6f1fd6491747914a010403009030c3a4ee251
Roberto Moreno <rmoreno@applaudostudios.dev>
2022-11-04 00:01:34 -0600

    Helper to update program tags

---
+++ app/Services/UpdateProgramService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Program;
+
+class UpdateProgramService
+{
+    public function __construct(protected CreateTagService $createTagService)
+    {
+    }
+
+    public function updateProgram(Program $program, array $attributes): Program
+    {
+        if ($attributes['tags']) {
+
+            $program->tags()->detach();
+
+            $tags = $attributes['tags'];
+
+            if (strpos($tags, ',') !== false) {
+
+                $tags = explode(",", $tags);
+
+                $tag_sync = [];
+
+                foreach ($tags as $tag) {
+                    $tags_sync[] = $this->createTagService->createTag($tag);
+                }
+
+                foreach ($tags_sync as $tag_sync) {
+                    $program->tags()->attach($tag_sync);
+                }
+            } else {
+
+                $tag_sync = $this->createTagService->createTag($tags);
+
+                $program->tags()->attach($tag_sync);
+            }
+
+            $program->save();
+        }
+
+        return $program;
+    }
+}
