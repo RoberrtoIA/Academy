46c9c1e11518082d95c0f3c6c2d248b6614846e0
Roberto Moreno <rmoreno@applaudostudios.dev>
2022-11-04 00:00:44 -0600

    Helper to check tags

---
+++ app/Services/CreateTagService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Tag;
+
+class CreateTagService
+{
+    public function createTag($tag_name): Tag
+    {
+        if ($tag = Tag::where('name', $tag_name)->first())
+        {
+            return $tag;
+        } else
+        {
+            return Tag::create(['name' => $tag_name]);
+        }
+    }
+}
