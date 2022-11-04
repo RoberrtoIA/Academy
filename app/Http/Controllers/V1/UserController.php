82464ab25b30aae6fdefab9c14837dc9be3b2868
Gaston Marsano <gmarsano@applaudostudios.dev>
2022-11-04 16:53:12 -0300

    Test it can list users

---
+++ app/Http/Controllers/V1/UserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return UserResource::collection(User::with('roles')->get());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ tests/Feature/V1/UserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature\V1;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
+
+    /** @test */
+    public function it_can_list_users()
+    {
+        $this->sanctumActingAsManager();
+        $count = User::count();
+
+        $this->get(route('api.v1.users.index'))
+            ->assertOk()
+            ->assertJsonCount($count, 'data');
+
+        User::factory()->create();
+
+        $this->get(route('api.v1.users.index'))
+            ->assertJsonCount(++$count, 'data');
+    }
+}
