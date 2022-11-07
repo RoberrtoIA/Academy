382a6a2cb3440cb96093d0a1641625bc67bcf342
Roberto Moreno <rmoreno@applaudostudios.dev>
2022-11-06 23:13:42 -0600

    Create question resource

---
+++ app/Http/Resources/QuestionResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QuestionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'question' => $this->question,
+            'created_at' => $this->whenNotNull($this->created_at ?? null),
+            'updated_at' => $this->whenNotNull($this->updated_at ?? null),
+            'deleted_at' => $this->whenNotNull($this->deleted_at ?? null),
+            'topic' => $this->whenLoaded('topic'),
+        ];
+    }
+}
