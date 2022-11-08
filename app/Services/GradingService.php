3431a101d8e4c7cbf900d95d8d5aac041f8d67b0
Roberto Moreno <rmoreno@applaudostudios.dev>
2022-11-08 16:37:41 -0600

    Create grading service

--- app/Services/GradingService.php
+++ app/Services/GradingService.php
@@ -3,8 +3,11 @@
 namespace App\Services;
 
 use App\Factories\GradingResourceFactory;
+use App\Http\Resources\GradingResource;
 use App\Models\Grading;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Validator;
 
 class GradingService
 {
@@ -16,6 +19,30 @@ class GradingService
     {
         $grading->snapshot = $this->resourceFactory->make($gradable, $gradableType)->resolve();
         $grading->save();
+
+        return $grading;
+    }
+
+    public function upsert(FormRequest $request, string $gradableType)
+    {
+        $attributes = $request->validated();
+
+        Validator::make($attributes, [
+            'gradable_id' => '|exists:' . $gradableType . ',id'
+        ])->validate();
+
+        $grading = Grading::updateOrCreate(
+            ['gradable_id' => $attributes['gradable_id'], 'gradable_type' => $gradableType],
+            ['comments' => $attributes['comments'], 'grade' => $attributes['grade']]
+        );
+
+        if (!$grading->snapshot) {
+            $this->takeSnapshot(
+                $grading,
+                $gradableType::find($request->input('gradable_id')),
+                $gradableType
+            );
+        }
 
         return $grading;
     }
