090537859098be9cfc88ef0a945239ba0fab665b
Gaston Marsano <gmarsano@applaudostudios.dev>
2022-11-11 18:53:36 -0300

    Test it takes homework snapshot

--- app/Listeners/TakeHomeworkSnapshot.php
+++ app/Listeners/TakeHomeworkSnapshot.php
@@ -3,29 +3,14 @@
 namespace App\Listeners;
 
 use App\Events\HomeworkStarted;
+use App\Services\HomeworkService;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
 class TakeHomeworkSnapshot
 {
-    /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function handle(HomeworkStarted $event, HomeworkService $service)
     {
-        //
-    }
-
-    /**
-     * Handle the event.
-     *
-     * @param  \App\Events\HomeworkStarted  $event
-     * @return void
-     */
-    public function handle(HomeworkStarted $event)
-    {
-        //
+        $service->takeSnapshot($event->assignment);
     }
 }
--- tests/Feature/V1/TakeHomeworkSnapshotTest.php
+++ tests/Feature/V1/TakeHomeworkSnapshotTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature\V1;
 
-use App\Events\HomeworkStarted;
-use App\Listeners\TakeHomeworkSnapshot;
+use App\Models\Module;
 use App\Models\Assignment;
+use App\Events\HomeworkStarted;
 use Illuminate\Support\Facades\Event;
+use App\Listeners\TakeHomeworkSnapshot;
+use App\Models\EvaluationCriteria;
+use App\Services\HomeworkService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TakeHomeworkSnapshotTest extends TestCase
@@ -24,5 +27,37 @@ class TakeHomeworkSnapshotTest extends TestCase
             HomeworkStarted::class,
             TakeHomeworkSnapshot::class
         );
+    }
+
+    /** @test */
+    public function it_takes_homework_snapshot()
+    {
+        $count = 4;
+        $module = Module::factory()->create();
+        $assignment = Assignment::factory()->for($module)->create();
+        $evaluationCriteria = EvaluationCriteria::factory($count)
+            ->for($module)
+            ->create();
+        $event = new HomeworkStarted($assignment);
+        $service = $this->app->make(HomeworkService::class);
+        $listener = new TakeHomeworkSnapshot;
+
+        $this->assertNull($assignment->homework_snapshot);
+
+        $listener->handle($event, $service);
+
+        $assignment->refresh();
+
+        $this->assertNotNull($assignment->homework_snapshot);
+        $this->assertArrayHasKey(
+            'id',
+            $assignment->homework_snapshot
+        );
+        $this->assertEquals($module->id, $assignment->homework_snapshot['id']);
+        $this->assertArrayHasKey(
+            'evaluation_criteria',
+            $assignment->homework_snapshot
+        );
+        $this->assertCount($count, $assignment->homework_snapshot['evaluation_criteria']);
     }
 }
