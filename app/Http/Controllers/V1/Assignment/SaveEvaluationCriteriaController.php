2c8c6d898bf865a2c78d4592a1420c41f802d631
Gaston Marsano <gmarsano@applaudostudios.dev>
2022-11-10 22:05:45 -0300

    Rename symbols

--- app/Http/Controllers/V1/Assignment/SaveEvaluationCriteriaController.php
+++ app/Http/Controllers/V1/Assignment/SaveEvaluationCriteriaController.php
@@ -1,19 +1,20 @@
 <?php
 
-namespace App\Http\Controllers\V1;
+namespace App\Http\Controllers\V1\Assignment;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SaveGradableRequest;
 use App\Http\Resources\GradingResource;
-use App\Models\Question;
+use App\Models\EvaluationCriteria;
 use App\Services\GradingService;
 
-class SaveQuestionController extends Controller
+class SaveEvaluationCriteriaController extends Controller
 {
     public function __invoke(SaveGradableRequest $request, GradingService $service)
     {
+
         return GradingResource::collection(
-            $service->upsert($request, Question::class)
+            $service->upsert($request, EvaluationCriteria::class)
         );
     }
 }
--- app/Http/Controllers/V1/Auth/AuthController.php
+++ app/Http/Controllers/V1/Auth/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\V1;
+namespace App\Http\Controllers\V1\Auth;
 
 use App\Models\User;
 use Illuminate\Http\Request;
